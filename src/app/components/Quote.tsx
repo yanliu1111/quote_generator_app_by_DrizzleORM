4758993af6d381b0933675585742dfadb880434e
yanliu1111 <liuyan0504@gmail.com>
2023-09-21 13:31:20 -0600

    feat: full quote page done

--- src/app/components/Quote.tsx
+++ src/app/components/Quote.tsx
@@ -22,20 +22,38 @@ export default function Quote(randomQuote: Quote) {
   const [fade, setFade] = useState(false); // fadein and out in css
 
   return (
-    <section className={`flex flex-col justify-center items-center transition-opacity ease-in-out duration-1000 ${fade ? 'opacity-0' : 'opacity-100'}`}>
-        <div className="flex flex-col justify-center items-center">
+    <section
+      className={`flex flex-col justify-center items-center transition-opacity ease-in-out duration-1000 ${
+        fade ? 'opacity-0' : 'opacity-100'
+      }`}
+    >
+      <div className='flex flex-col justify-center items-center'>
         <button
-                    type="submit"
-                    className="p-3 mb-4 text-xl rounded-2xl text-black border-solid border-black border-2 max-w-xs bg-slate-200 hover:cursor-pointer hover:bg-white mt-6"
-                    onClick={() => {
-                        setFade(true)
-                        setTimeout(() => router.refresh(), 1000) //router.refresh() is a nextjs function that refreshes the page
-                        setTimeout(() => setFade(false), 1200)
-                    }}
-                >
-                    Refresh Quote
-                </button>
+          type='submit'
+          className='p-3 mb-4 text-xl rounded-2xl text-black border-solid border-black border-2 max-w-xs bg-slate-200 hover:cursor-pointer hover:bg-white mt-6'
+          onClick={() => {
+            setFade(true);
+            setTimeout(() => router.refresh(), 1000); //router.refresh() is a nextjs function that refreshes the page
+            setTimeout(() => setFade(false), 1200);
+          }}
+        >
+          Refresh Quote
+        </button>
+        <p className={`text-2xl text-center mt-4 ${poppins.className}`}>
+          Author: {randomQuote.author}
+        </p>
 
+        <p className={`text-2xl text-center mt-4 ${poppins.className}`}>
+          Category: {randomQuote.category}
+        </p>
+      </div>
+      <div className='grow mt-24'>
+        <p
+          className={`text-5xl text-center max-w-3xl leading-normal ${roboto.className}`}
+        >
+          {randomQuote.quote}
+        </p>
+      </div>
     </section>
-  )
+  );
 }
